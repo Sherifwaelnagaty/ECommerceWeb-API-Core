b1ddcea9c32903d70180398f75aa36cf0225b6dd
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-04-16 05:30:33 +0200

    Adding 4 new projects Core, Dependency Injection, Services, Repository

---
+++ DependencyInjection/DependencyConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection
+{
+    public static class DependencyConfig
+    {
+        public static IServiceCollection ConfigureDependencies(IServiceCollection Services)
+        {
+            
+
+            return Services;
+        }
+    }
+}
