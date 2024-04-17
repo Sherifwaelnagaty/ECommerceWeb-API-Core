3bb78981f0c053506fdeac20e452005a204c8c4f
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-04-17 04:09:29 +0200

    Add Enums, Products Model, ProductsDTO

---
+++ Core/Domain/Product.cs
@@ -0,0 +1,25 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public string Colors { get; set; }
+        public Sizes Sizes { get; set; }
+        public string Price { get; set; }
+        public string Images { get; set; }
+        public string Reviews { get; set; }
+        public string Quantity { get; set; }
+    }
+}
---
+++ Core/Enums/Enums.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Enums
+{
+
+    public enum Days
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+    }
+    public enum Gender { Male, Female, }
+    public enum DiscountType
+    {
+        percentage,
+        value,
+    }
+    public enum UserRole
+    {
+        Doctor,
+        Patient,
+        Admin
+    }
+    public enum BookingState
+    {
+        Pending,
+        Completed,
+        Cancelled
+    }
+    public enum Sizes
+    {
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL
+    }
+
+}
