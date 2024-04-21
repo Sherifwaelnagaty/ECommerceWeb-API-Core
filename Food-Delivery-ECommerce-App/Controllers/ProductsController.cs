b40ed36df9a611333a5c55056c33b4e23af9b5ee
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-04-21 20:42:46 +0200

    Implement AddProduct Function in Product Controllers

--- Food-Delivery-ECommerce-App/Controllers/ProductsController.cs
+++ Food-Delivery-ECommerce-App/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Core.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services;
 
@@ -23,6 +24,32 @@ namespace Controllers
 
             return _services.GetAllProducts(page, pageSize, search);
         }
+        [HttpPost("")]
+        public async Task<IActionResult> AddProduct(ProductsDTO products)
+        {
+            if (products == null)
+            {
+                return BadRequest("Price and appointment are required");
 
+            }
+
+            if (products.Price <= 0)
+            {
+                return BadRequest("Invalid Price");
+            }
+
+            if (products.Quantity >= 0)
+            {
+                return BadRequest("Quantity have to me more than 0 ");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return await _services.AddProduct(products);
+               
+        }
     }
 }
