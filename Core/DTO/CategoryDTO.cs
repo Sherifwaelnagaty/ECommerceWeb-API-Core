8d6e6ef69a66949c2cd730bfef7ee60fd82c2d29
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-04-30 15:36:25 +0300

    Implement Brand, Category, Color DTOs

---
+++ Core/DTO/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class CategoryDTO
+    {
+        public string Name;
+        public List<Image> Images;
+    }
+}
