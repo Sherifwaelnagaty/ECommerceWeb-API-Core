74d2094cadcc50cc8a925b4135905c4f00df7e1c
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-04-30 15:27:42 +0300

    Implement Brand, Category, Color Models

---
+++ Core/Domain/Brand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("FK_Color_ApplicationUser_UserId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("FK_Color_Products_ProductId")]
+        public int ProductId { get; set; }
+
+    }
+}
---
+++ Core/Domain/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Images { get; set; }
+
+        [ForeignKey("FK_Color_ApplicationUser_UserId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("FK_Color_Products_ProductId")]
+        public int ProductId { get; set; }
+
+    }
+}
