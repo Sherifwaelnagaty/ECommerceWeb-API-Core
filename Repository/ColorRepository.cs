89fe53ec7f0c55988024cc952846de385b56d9c7
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-05-02 12:36:10 +0300

    Implement GetAllColors Function in ColorRepository

--- Repository/ColorRepository.cs
+++ Repository/ColorRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Core.Domain;
+using Core.DTO;
+using Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,42 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    internal class ColorRepository
+    public class ColorRepository : BaseRepository<Color>, IColorRepository
     {
+        public ColorRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+        public IActionResult GetAllColors(int Page, int PageSize, Func<ColorDTO, bool> criteria = null)
+        {
+            try
+            {
+                IEnumerable<ColorDTO> ColorsInfo = _context.Set<Color>().Select(p => new ColorDTO
+                {
+                    Name = p.Name,
+                });
+
+                if (criteria != null)
+                {
+                    ColorsInfo = ColorsInfo.Where(criteria);
+                }
+
+                if (Page != 0)
+                    ColorsInfo = ColorsInfo.Skip((Page - 1) * PageSize);
+
+                if (PageSize != 0)
+                    ColorsInfo = ColorsInfo.Take(PageSize);
+
+                return new OkObjectResult(ColorsInfo.ToList());
+
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"There is a problem during getting the data {ex.Message}")
+                {
+                    StatusCode = 500
+                };
+            }
+        }
     }
 }
