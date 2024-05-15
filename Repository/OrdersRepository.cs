326b7833a8118cdf61fd633ddf4f191df49a8e5e
Sherifwaelnagaty <Sherifwael0@gmail.com>
2024-05-15 21:49:25 +0300

    Create OrdersRepository and Reviews Repository

--- Repository/OrdersRepository.cs
+++ Repository/OrdersRepository.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Core.Domain;
+using Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class OrdersRepository
+    public class OrdersRepository : IOrdersRepository
     {
+        public Task<IActionResult> Add(Orders entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete(Orders entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetAvgOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Orders GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Orders GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetMinMaxOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetSalesSumCrtl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsExist(Expression<Func<Orders, bool>> criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Update(Orders entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repository/ReviewsRepository.cs
+++ Repository/ReviewsRepository.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Core.Domain;
+using Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class ReviewsRepository
+    public class ReviewsRepository : IReviewsRepository
     {
+        public Task<IActionResult> Add(Reviews entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete(Reviews entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reviews GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reviews GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsExist(Expression<Func<Reviews, bool>> criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Update(Reviews entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
